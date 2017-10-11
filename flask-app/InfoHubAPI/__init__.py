91ebfdfa46a66ee130c0483ad56d204b6749875a
Anton Levholm <anton.badminton@gmail.com>
2017-10-11 20:40:52 +0200

    some code cleanup

--- flask-app/InfoHubAPI/__init__.py
+++ flask-app/InfoHubAPI/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response
 from .vasttrafik import get_vasttrafik_json
-from .darksky import getDarkSkyJSON
-from .twitter import getTwitterJSON
+from .darksky import get_darksky_json
+from .twitter import get_twitter_json
 app = Flask(__name__)
 
 
@@ -14,12 +14,12 @@ def vasttrafik(id=None):
 @app.route('/darksky/<location>')
 # location format: [latitude],[longitude]
 def darksky(location=None):
-    resp = Response(getDarkSkyJSON(location))
+    resp = Response(get_darksky_json(location))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 @app.route('/twitter/<user>')
 def twitter(user=None):
-    resp = Response(getTwitterJSON(user))
+    resp = Response(get_twitter_json(user))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
