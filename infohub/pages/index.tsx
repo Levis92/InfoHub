df733ff75dae8d7bd165fd38362321ae9de67ee4
Anton Levholm <anton.badminton@gmail.com>
2021-04-11 12:11:26 +0200

    Remove news widget from home page

--- infohub/pages/index.tsx
+++ infohub/pages/index.tsx
@@ -1,12 +1,12 @@
 import { HomeLayout } from 'layout';
-import { Clock, News, Twitter, Weather, Vasttrafik } from 'widgets';
+import { Clock, Twitter, Weather, Vasttrafik } from 'widgets';
 
 export default function Home() {
   return (
     <HomeLayout>
       <Twitter />
       <Vasttrafik />
-      <Clock top={<News />} />
+      <Clock />
       <Weather />
     </HomeLayout>
   )
